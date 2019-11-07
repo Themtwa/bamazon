4eed0383abf70d6f08cb3e091efd11d5b6940dea
Matthew Tarbet <goatdaddy08@yahoo.com>
2019-11-07 11:52:35 -0700

    uploading while I have internet

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,6 +1,7 @@
 const inquirer = require('inquirer');
 const mysql = require('mysql');
 const chalk = require('chalk');
+require('console.table');
 
 const connection = mysql.createConnection({
     host: 'localhost',
@@ -9,3 +10,23 @@ const connection = mysql.createConnection({
     password: 'notmynormal',
     database: 'bamazon'
 });
+connection.connect(function(error){
+    if (error) throw error;
+    loadProducts();
+});
+
+function loadProducts() {
+    connection.query("SELECT * FROM products", function (error, response) {
+        if (error) throw error;
+        console.table(response)
+        customerItemPrompt(response);
+    });
+}
+
+function customerItemPrompt(inventory) {
+    inquirer.prompt([
+        {
+            
+        }
+    ])
+}
